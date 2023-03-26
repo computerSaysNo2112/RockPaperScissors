27a542da19d330065799d1d5f1cea79f512a4f04
computerSaysNo2112 <Lovishhgz@gmail.com>
2023-03-26 16:10:28 -0400

    Added scoreboard, started coding js

---
+++ updatedScript.js
@@ -0,0 +1,24 @@
+// Retrieve Buttons from html
+const rockBtn = document.querySelector('.rock');
+const paperBtn = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.scissors');
+
+// Create Score info
+
+let playerChoice;
+
+rockBtn.addEventListener('click', () => {
+    playerChoice = 'rock';
+    console.log('rock');
+})
+
+
+
+
+
+
+
+
+
+
+
